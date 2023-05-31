9991872fa91482a3e0e853ad3159187b8e0f54c1
OlumideEmmanuel <olumideemmanuel17@gmail.com>
2023-05-31 09:21:57 -0700

    2

--- Converter.py
+++ Converter.py
@@ -1,11 +1,27 @@
+
 import math
+
+units = ["B", "KB", "MB", "BG", "TB"]
+
+units_to_use = {
+    "B": 1, 
+    "KB": 1024, 
+    "MB": 1024 **2, 
+    "BG": 1024 **3, 
+    "TB":1024 **4,
+}
+
+
+def func1 ():
+    1024
+
 
 size = float(input("Enter the size: "))
 unit_from = input("Enter the unit to convert from (B, KB, MB, BG, TB): ")
 unit_to = input("Enter the unit to convert to (B, KB, MB, BG, TB): ")
 
-
-
+if unit_from not in units or unit_to not in units:
+    raise ValueError("Invalid units")
 
 if unit_from == "B" and unit_to == "B":
     print(size)
@@ -18,7 +34,7 @@ elif unit_from == "BG" and unit_to == "BG":
 elif unit_from == "TB" and unit_to == "TB":
     print(size)
 elif unit_from == "B" and unit_to == "KB":
-    KB = size / 1024
+    KB = size / func1
     print(f"Converted size: {KB} KB")
 elif unit_from == "KB" and unit_to == "B":
     B = size * 1024
@@ -77,3 +93,5 @@ elif unit_from == "BG" and unit_to == "TB":
 elif unit_from == "TB" and unit_to == "BG":
     BG = size * 1024 * 1024
     print(f"Converted size: {BG} BG")
+# else:
+#     print("Error Input the correct")
