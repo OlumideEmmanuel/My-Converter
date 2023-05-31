112598746c58e4c03e8dbca71652fef2ea3b1fd8
OlumideEmmanuel <olumideemmanuel17@gmail.com>
2023-05-31 08:45:55 -0700

    First Push

---
+++ Converter.py
@@ -0,0 +1,79 @@
+import math
+
+size = float(input("Enter the size: "))
+unit_from = input("Enter the unit to convert from (B, KB, MB, BG, TB): ")
+unit_to = input("Enter the unit to convert to (B, KB, MB, BG, TB): ")
+
+
+
+
+if unit_from == "B" and unit_to == "B":
+    print(size)
+elif unit_from == "KB" and unit_to == "KB":
+    print(size)
+elif unit_from == "MB" and unit_to == "MB":
+    print(size)
+elif unit_from == "BG" and unit_to == "BG":
+    print(size)
+elif unit_from == "TB" and unit_to == "TB":
+    print(size)
+elif unit_from == "B" and unit_to == "KB":
+    KB = size / 1024
+    print(f"Converted size: {KB} KB")
+elif unit_from == "KB" and unit_to == "B":
+    B = size * 1024
+    print(f"Converted size: {B} B")
+elif unit_from == "B" and unit_to == "MB":
+    MB = size / 1024 * 1024
+    print(f"Converted size: {MB} MB")
+elif unit_from == "MB" and unit_to == "B":
+    B = size * 1024 * 1024
+    print(f"Converted size: {B} B")
+elif unit_from == "B" and unit_to == "BG":
+    BG = size / 1024 * 1024 * 1024
+    print(f"Converted size: {BG} BG")
+elif unit_from == "BG" and unit_to == "B":
+    B = size * 1024 * 1024 * 1024
+    print(f"Converted size: {B} B")
+elif unit_from == "B" and unit_to == "TB":
+    TB = size / 1024 * 1024 * 1024 * 1024
+    print(f"Converted size: {TB} TB")
+elif unit_from == "TB" and unit_to == "B":
+    B = size * 1024 * 1024 * 1024 * 1024
+    print(f"Converted size: {B} B")
+elif unit_from == "KB" and unit_to == "MB":
+    MB = size / 1024 
+    print(f"Converted size: {MB} MB")
+elif unit_from == "MB" and unit_to == "KB":
+    KB = size * 1024 
+    print(f"Converted size: {KB} KB")
+elif unit_from == "KB" and unit_to == "BG":
+    BG = size / 1024 * 1024
+    print(f"Converted size: {BG} BG")
+elif unit_from == "BG" and unit_to == "KB":
+    KB = size * 1024 * 1024
+    print(f"Converted size: {KB} KB")
+elif unit_from == "KB" and unit_to == "TB":
+    TB = size / 1024 * 1024 * 1024
+    print(f"Converted size: {TB} TB")
+elif unit_from == "TB" and unit_to == "KB":
+    KB = size * 1024 * 1024 * 1024
+    print(f"Converted size: {KB} KB")
+elif unit_from == "MB" and unit_to == "BG":
+    BG = size / 1024 * 1024
+    print(f"Converted size: {BG} BG")
+elif unit_from == "BG" and unit_to == "MB":
+    MB = size * 1024 * 1024
+    print(f"Converted size: {MB} MB")
+elif unit_from == "MB" and unit_to == "TB":
+    TB = size / 1024
+    print(f"Converted size: {TB} TB")
+elif unit_from == "TB" and unit_to == "MB":
+    MB = size * 1024
+    print(f"Converted size: {MB} MB")
+elif unit_from == "BG" and unit_to == "TB":
+    TB = size / 1024 * 1024
+    print(f"Converted size: {TB} TB")
+elif unit_from == "TB" and unit_to == "BG":
+    BG = size * 1024 * 1024
+    print(f"Converted size: {BG} BG")
